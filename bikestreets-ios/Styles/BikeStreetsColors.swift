35cd959c099494b77a5827b59de9664f5c5ce512
Matt Robinson <mattrob@hey.com>
2023-12-22 17:33:38 -0700

    Render current route below Vamos network (#20)
    - Update color of current route and render below the Vamos network.
    - Update the light mode map style to Avi’s custom format.
    
    Signed-off-by: Matt Robinson <mattrob@hey.com>

--- bikestreets-ios/Styles/BikeStreetsColors.swift
+++ bikestreets-ios/Styles/BikeStreetsColors.swift
@@ -11,5 +11,11 @@ import UIKit
 extension UIColor {
   static let vamosBlue = UIColor(red: 53.0 / 255.0, green: 90.0 / 255.0, blue: 168.0 / 255.0, alpha: 1)
   static let vamosPurple = UIColor(red: 151.0 / 255.0, green: 27.0 / 255.0, blue: 138.0 / 255.0, alpha: 1)
+  static let vamosYellow = UIColor(
+    red: 254.0 / 255.0,
+    green: 205.0 / 255.0,
+    blue: 59.0 / 255.0,
+    alpha: 1
+  )
 }
 
